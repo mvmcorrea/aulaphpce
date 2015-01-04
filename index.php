75f909ed4910fa886f2c4e0b4b25a94b42e3daa4
Marcus Correa <marcuscorrea@live.com>
2015-01-04 20:34:24 -0200

    Primeiro Commit Aulas PHP

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>Curso de GIT Git Hub</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <![endif]-->
+</head>
+<body>
+    <h1>Olá, mundo!</h1>
+    <!-- Javascript -->
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+    <?php
+        phpinfo();
+    ?>
+</body>
+</html>
